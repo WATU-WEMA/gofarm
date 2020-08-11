ed0271b9cd61ab540573aae28d37f08ba3686a08
Waltertz <67097588+Waltertz@users.noreply.github.com>
2020-08-11 08:22:08 +0300

    Add files via upload

---
+++ nyanya.php
@@ -0,0 +1,207 @@
+<?php
+session_start();
+echo '<br><a href="farmgo.php?action=logout" style="color: inherit;font-size:50px;">LOG OUT</a>'
+?>
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+  <title>NYANYA</title>
+  <style>
+  body
+  {
+      background-color: slateblue;
+      background-repeat:no repeat;
+      background-size:cover;
+      color: white ;
+  }
+  h1
+  {
+      text-align: center;
+      color:white ;
+  }
+   #main-content
+    {
+            width: 780px;
+             float: left;
+     }
+   #news
+    {
+         margin-left: 20px;
+          float: left;
+         width: 180px;
+     }
+     ul li a
+     {
+         text-decoration: none;
+     }
+       li a,a:visited, a:hover, a:active
+   {
+       color: inherit;
+   }
+   h2
+   {
+       padding-left: 70px;
+   }
+
+  </style>
+</head>
+
+<body>
+
+   <h1>NYANYA</h1>
+   <hr>
+     <div id="main-content">
+      <div class="article">
+      <h2>KILIMO CHA NYANYA</h2>
+      <img src="nyanyaaaa.jpg">
+      <p>Nchini Tanzania zao la nyanya hulimwa karibu katika mikoa yote hasa ya Arusha, Kilimanjaro, Morogoro, Iringa, Mwanza, Dodoma, Tanga, Pwani na Mbeya.
+Nyanya ni zao la chakula na biashara.
+Kilimo cha zao hili hufanyika majira yote (masika na kiangazi), na faida hupatikana
+haraka na ni ya kuridhisha ikilinganishwa na mazao mengine. <br>
+      </p>
+           </div>
+               <div class="article">
+               <h2 id="mahitaji">MAHITAJI</h2><br>
+               <img src="nyany.jpg" width="50%" height="50%">
+
+             <p>  Udongo<br>
+Nyanya hustawi kwenye aina zote za udongo kuanzia udongo wa kichanga, mwepesi na
+tifutifu hadi udongo wa mfinyanzi, ili mradi uwe na mboji ya kutosha.<br>
+Udongo unaofaa
+zaidi ni ule wa kitifutifu na wenye mboji nyingi.
+Hali kadhalika udongo wenye uchachu
+wa wastani yaani pH 6.0 - 7.0 ni mzuri kwa kilimo cha zao hili.<br>
+· Hali ya Hewa<br>
+Nyanya hustawi vizuri zaidi kwenye mazingira ya joto la wastani kuanzia nyuzi joto 18-30
+sentigreti.
+Mvua nyingi husababisha mlipuko wa magonjwa ya ukungu kama vile Baka jani
+chelewa n.k.) Joto likizidi husababisha matawi ya matunda kuwa machache.
+Vilevile joto
+jingi, mwanga kidogo, pamoja na hali ya unyevunyevu husababisha magonjwa mengi
+kuenea na mmea kuwa na majani mengi na matunda kidogo.
+Joto kidogo hufanya mimea
+kutoa matawi mengi na matunda madogo ambayo huchelewa kukomaa.
+Zao hili hupendelea mvua za wastani pamoja na kipindi kirefu chajua.
+Hali ya mvua nyingi pamoja
+na baridi kali husababisha kuenea kwa magonjwa na wadudu waharibifu.</p>
+               </div>
+          <div class="article">
+               <h2 id="magonjwa">MAGONJWA</h2>
+               <img src="nyanyadis.jpg">
+
+
+               <p>
+
+                Bakajani chelewa (Late blight)<br>
+Ugonjwa huu huenenzwa na vimelea vya fangasi.
+Husababishwa na hali ya hewa hasa ya unyevunyevu , ukungu , huenezwa na upepo. Majani, shina na matunda hushambuliwa.
+Dalili za ugonjwa huu huanza kuonekana kwenye jani la chini. Majani na shina huwa na mabaka
+makubwa yenye rangi nyeusi au kikahawia nzito.
+Katika sehemu zenye hali ya hewa ya
+unyevunyevu, majani huoza na hatimaye ukungu mweupe na kijivu huonekana chini ya jani.
+Majani haya baadaye hukauka na kuwa kama yaliyounguzwa na moto. Mashina pia huwa na
+mabaka ya kikahawia.
+Matunda yaliyopatwa na ugonjwa huu huwa na madoa ya rangi ya kijani
+iliyochanganyika na kikahawia. Baadaye madoa haya huwa makubwa na hatimaye huoza. Ukipasua tunda lililoathiriwa utaona weusi.
+Nyanya zenye ugonjwa huu zikihifadhiwa kwenye
+sehemu yenye unyevu mwingi hutoa ukungu mweupe.<br>
+Udhibiti<br>
+Nyunyiza dawa ya kuzuia ukungu hasa wakati wa masika,
+Dawa zinazopendekezwa ni
+Ridomil, Dithane 45, Bravo, Funguran, milraz, Topsin M 70, Cupric Hydroxide
+(Champion) na Copper Oxychloride (Cupro).</p>
+               </div>
+          <div class="article">
+               <h2 id="masoko">MASOKO</h2>
+               <img src="nya.jpg"><br>
+
+               <p>maelezo ya masoko</p>
+               </div>
+           <div class="article">
+               <h2 id="ziada">ZIADA</h2><br>
+               <img src="nyaa.jpg"><br>
+               <p> KUTUNZA SHAMBA:<br>
+ 1. Kuweka Matandazo:<br>
+Baada ya kuhamishia miche shambani, weka matandazo ili kuhifadhi unyevu na kuzuia magugu. Matandazo pia husaidia kuzuia mmomonyoko wa udongo na kutunza rutuba.
+Vile vile hupunguza uzito wa matone ya mvua na kuweka matunda katika hali ya usafi.<br>
+2. Kusimika Miti na Kuegesha Mimea:<br>
+Kazi ya kusimika miti na kuegesha mimea hufanyika wiki ya pili baada ya kupandikiza.
+Miti ya kuegesha mimea iwe imara, na ambayo haiozi haraka.
+Simika miti kiasi cha umbali wa sentimita tano kutoka kwenye mmea.
+Wakati wa kusimika mti hakikisha kwamba mmea unakuwa upande
+wa ndani ili kuzuia mmea usiumie wakati wa kuchuma au kunyunyuzia dawa.
+Kisha kwa kutumia mkono shindilia mti sentimita 20 hadi 50 kwenda chini ya ardhi.
+Mti uwe na urefu wa
+mita moja na nusu hadi mbili na unene wa sentimita mbili hadi tatu.
+<br>
+3. Kufunga Nyanya Kwenye Mti<br>
+Aina ndefu ya nyanya ni lazima ifungiwe kwenye mti ili kuzuia mmea usitambae chini.
+Mimea iliyofungwa kwenye mti hurahisisha unyunyiziaji wa dawa, umwagiliaji na uchumaji wa
+matunda.
+Ili kupata mazao yaliyo bora na makubwa ondoa matawi yote na kuacha mashina mawili.
+Yafungie mashina hayo kwenye mti ili mimea iweze kutambaa na kuwa wima.
+Funga kamba katika umbo la nane chini ya kila jani la tatu na kila baada ya sentimita 20 hadi 25.
+<br>
+4. Kupunguza Matawi:<br>
+Kupunguza matawi ni muhimu na hasa kwa aina ndefu kama vile Money Maker.
+Kazi hii hufanyika mara moja kwa wiki.
+Wakati mzuri wa kupunguzia ni asubuhi.
+Ondoa matawi yote na acha shina moja au mawili.
+Usitumie kisu kupunguzia matawi hali matawi yavunjwe kwa kutumia mkono.
+Utumiaji wa kisu hueneza magonjwa yanayosababishwa na virusi au bacteria.
+<br>
+5. Kuondoa Majani<br>
+Ondoa majani yote yanayoonyesha dalili za magonjwa au kushambuliwa na wadudu na yale yote
+yaliyozeeka ili kupunguza hali ya unyevu na kuruhusu hewa ya kutosha kwa mimea.
+Ondoa majani mawili au matatu yaliyo chini ya ngazi ya kwanza ya matunda kisha yachome au yafukie
+<br>
+6. Kukata Kilele<br>
+Kwa kawaida aina ndefu ya nyanya huzaa ngazi zaidi ya sita za matunda, lakini mmea una
+uwezo wa kubeba ngazi tano hadi sita tu.
+Hivyo ni muhimu kukata sehemu ya juu ya mmea (kilele) ili kusimamisha ukuaji wake.
+Kata kilele mimea ifikiapo ngazi hizo.
+Jani moja liachwe juu ya ngazi ya mwisho (ya tano au ya sita).
+Machipukizi yote yanayojitokeza pembeni hayana budi kuondolewa ili kupata matunda
+makubwa na yatakayoiva upesi.
+
+
+<br>
+7. Palizi<br>
+Palizi katika shamba la nyanya ifanywe kwa uangalifu ili kuepuka kukata mizizi.
+Punguza palizi ya mara kwa mara kwa kuweka matandazo kuzuia uotaji wa magugu.
+Wakati wa kupalilia, pandishia udongo kwenye shina.
+<br>
+8. Kumwagilia Kwa kawaida nyanya hutoa mavuno mengi wakati wa kiangazi kuliko wakati wa masika. Mwagilia maji ya kutosha mara mbili kwa wiki hasa matunda ya kwanza yanapoanza kutunga.
+Kumwagilia maji mengi sana au kidogo sana kunasababisha matunda yawe na hitilafu ya
+kupasuka.
+Wakati wa mwanzo epuka kumwagilia maji mengi ili mimea isididimie.
+<br>
+9. Kuweka Mbolea ya Kukuzia<br>
+Mbolea ya kukuzia huwekwa mara mbili.
+Mara ya kwanza weka gramu tano za S/A auCAN kwa
+kila mche katika wiki ya pili hadi ya nne baada ya kupandikiza.
+Rudia tena kuweka kiasi hicho
+hicho wakati matunda ya ngazi ya kwanza yanapoanza kuiva.</p>
+
+               <p>Machungwa kwa kawaida hulimwa kwa ajili ya biashara na hulimwa maeneo mengi duniani. Wazalishaji wa machungwa wanaoongoza duniani ni Brazili, Marekani na Meksiko. Machungwa huathirika kwa haraka sana na ukungu hivyo huhitaji matunzo mazuri pindi joto dogo sana linapotegemewa</p>
+               </div>
+
+               </div>
+<div id="news">
+<div class="news-article">
+    <h3>SOMO</h3>
+    <ul>
+        <li><a href="#mahitaji"><h4>MAHITAJI</h4></a></li>
+        <li><a href="#magonjwa"><h4>MAGONJWA</h4></a></li>
+        <li><a href="#masoko"><h4>MASOKO</h4></a></li>
+        <li><a href="#ziada"><h4>ZIADA</h4></a></li>
+    </ul>
+
+    </div>
+    </div>
+
+</body>
+
+</html>
