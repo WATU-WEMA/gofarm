5ed17c2170b8b363703b251dbb4f9361b729e5c3
Waltertz <67097588+Waltertz@users.noreply.github.com>
2020-08-11 08:20:24 +0300

    Add files via upload

---
+++ embe.php
@@ -0,0 +1,141 @@
+<?php
+session_start();
+echo '<br><a href="farmgo.php?action=logout" style="color: inherit;font-size:50px;">LOG OUT</a>'
+?>
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+  <title>EMBE</title>
+  <style>
+  body
+  {
+      background-color: violet;
+      background-repeat:no repeat;
+      background-size:cover;
+      color: white ;
+  }
+  h1
+  {
+      text-align: center;
+      color:white ;
+  }
+   #main-content
+    {
+            width: 780px;
+             float: left;
+     }
+   #news
+    {
+         margin-left: 20px;
+          float: left;
+         width: 180px;
+     }
+     ul li a
+     {
+         text-decoration: none;
+     }
+       li a,a:visited, a:hover, a:active
+   {
+       color: inherit;
+   }
+   h2
+   {
+       padding-left: 70px;
+   }
+
+  </style>
+</head>
+
+<body>
+
+   <h1>EMBE</h1>
+   <hr>
+     <div id="main-content">
+      <div class="article">
+      <h2>KILIMO CHA EMBE</h2>
+      <img src="embe.jpeg">
+      <p>Embe ni moja ya zao la biashara ambalo lina faida kubwa endapo litalimwa kwa kufuata masharti na<br> mavuno kupatikana
+      <br>
+      Zao la embe hustawi vizuri katika maeneo ya tambarare na miinuko kati ya futi 0-600 usawa wa bahari.
+
+Kiwango cha wastani cha mvua inayohitajika kwa kilimo cha embe ni mililita 650 hadi 1850 kwa mwaka.
+
+Miembe hustawi katika udongo wenye uchachu (ph) kati ya 5.5 na 7.2. udongo wenye chachu zaidi huwa na upungufu wa madini mbalimbali ikiwemo zinki, chuma, fosforas na kalsham.
+<br>
+Upandaji<br>
+Panda miche ya miembe katika misimi ya mvua za mwaka, au wakati wowote iwapo inatumika njia ya umwagiliaji kwa vipindi visivyo na mvua. Weka maji ya kutosha wakati wa upandaji ili kuimarisha mshikamano wa udongo na unyevu.
+     <br>
+     Ukataji<br>
+Ukatiaji hufanyika katika hatua ya kwanza ya ukuaji miti ukifikia urefu wa sentimiti 60-100, ukatiaji wa kwanza utatofatiana na ukatiaji wa pili na kuendelea.
+      </p>
+           </div>
+               <div class="article">
+               <h2 id="mahitaji">MAHITAJI</h2>
+               <img src="embe1.png"><img src="embe2.png">
+
+               <p>Kuandaa mashimo<br>
+Kuchimba mashimo mapana kutafanya udongo uwe laini ili kusaidia mizizi kuzaliwa kwa wingi, upana wa shimo uwe sentimita 60x60 upana kwa urefu, kwa maeneo yenye udongo mgumu na miamba ya mawe kipimo kiwe sentimita 100x100 upana na urefu.
+
+Uchimbaji wa mashimo uwe wa kutengeneza udongo katika sehemu mbili. Sehemu ya kwanza ni udongo wa juu wa kina cha sentimita 30 na sehemu ya pili ni udongo sentimita zilizobaki za kina cha shimo.</p>
+               <br>
+               Kipimo<br>
+Miembe hupandwa katika vipimo tofauti kutegemeana mipango ya matumizi ya ardhi husika na miundombinu ya shamba. Upandaji unaweza kuwa wa miti 160 hadi 1300 kwa hekta moja. Vipimo vya umbali kati ya mti ana mti vifuate mistari iliyonyooka.
+Kipimo cha wastani kwa wakulima wadogo na wakati ni mita 8x8, mita 6x8, mita 4x8, mita 4x7 au mita 5x7. Wastani kwa hekta ni miembe 200-400 ujazo wa vipimo vingine vinaweza kufikia miti 1200 na Zaidi kwa hekta. Matumizi kwa vipimi hivyo vinahitaji ushauri wa mtaala, kabla ya kutumia.
+     <br>
+
+               </div>
+          <div class="article">
+               <h2 id="magonjwa">MAGONJWA</h2>
+               <img src=".\imagesss\embedis1.jpeg"><img src=".\imagesss\embedis2.jpeg">
+
+
+               <p>Magonjwa na wadudu<br>
+Miembe hushambuliwa na magonjwa kama Ubwiru unga (powder mildew), Chule
+(Antharacnose), magonjwa mengine hutokea kwa mara chache sana. Wadudu mbalimbali wanaoshambulia mazao tofauti, hushambulia miti ya miembe kama Vidukari, Vung’ata, Kifaurongo, Mbawa kavu, nk.
+<br>
+Udhibiti wa magonjwa na wadudu<br>
+Fanya usafi wa palizi shambani kila inapolazimu ili kupnguza vyanzo wa wadudu na magonjwa, matumizi ya dawa yanafanyika kila dalili za ugonjwa zaitakapo jitokeza au mashambulizi ya wadudu yatakapoonekana.
+</p>
+               </div>
+          <div class="article">
+               <h2 id="masoko">MASOKO</h2>
+
+               <p>maelezo ya masoko</p>
+               </div>
+           <div class="article">
+               <h2 id="ziada">ZIADA</h2>
+
+               <p>Urutubishaji na mbolea<br>
+Mbolea ni muhimu katika uzalishaji wa embe, mbolea huendelea kutumika
+wakati wa kukuza miche na kuzalisha matunda. Mbolea zinazotumika ni N.PK, MOP, Urea, Minjingu, Samadi, Mboji, Chokaa na virutubisho visaidizi.
+
+Mbolea zitawekwa kulingana na mahitaji ya udongo na kwa wakati maalum, uwekaji holela wa mbolea unaweza ukaathiri uzalishaji utakaotokana na ukosefu wa maua.
+<br>
+Uvunaji na utunzaji<br>
+Embe zitunzwe zikiwa zimekomaa, embe lililokomaa lina umbo tofauti kulingana na aina ya embe, kiashiria kikuu ni embe kubonya kwa eneo la kikonyo, alama nyingine ni kubadilika kwa rangi ya ndani ya nyama ya embe kutoka nyeupe kuwa njano hafifu.
+
+Embe zinazovunwa zioshewe kwa maji ya moto na kuhifadhiwa sehemu kavu na
+salama, chagua embe na kuziweka katika madaraja mfano daraja la 1 na daraja 2. Hii itakusaidia kujua ipi embe ni tayari kwa kupeleka sokoni.
+</p>
+               </div>
+
+               </div>
+<div id="news">
+<div class="news-article">
+    <h3>SOMO</h3>
+    <ul>
+        <li><a href="#mahitaji"><h4>MAHITAJI</h4></a></li>
+        <li><a href="#magonjwa"><h4>MAGONJWA</h4></a></li>
+        <li><a href="#masoko"><h4>MASOKO</h4></a></li>
+        <li><a href="#ziada"><h4>ZIADA</h4></a></li>
+        <img src="embe3.jpg">
+    </ul>
+
+    </div>
+    </div>
+
+</body>
+
+</html>
