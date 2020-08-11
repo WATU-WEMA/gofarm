2d2205db5a4a16614f96cc0c04238c90109ee518
Waltertz <67097588+Waltertz@users.noreply.github.com>
2020-08-11 08:18:30 +0300

    Add files via upload

---
+++ CHUNGWA.php
@@ -0,0 +1,150 @@
+<?php
+session_start();
+echo '<br><a href="farmgo.php?action=logout" style="color: inherit;font-size:50px;">LOG OUT</a>'
+?>
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+  <title>CHUNGWA</title>
+  <style>
+  body
+  {
+      background-color: orange;
+      background-repeat:no repeat;
+      background-size:cover;
+      color: white ;
+  }
+  h1
+  {
+      text-align: center;
+      color:white ;
+  }
+   #main-content
+    {
+            width: 780px;
+             float: left;
+     }
+   #news
+    {
+         margin-left: 20px;
+          float: left;
+         width: 180px;
+     }
+     ul li a
+     {
+         text-decoration: none;
+     }
+       li a,a:visited, a:hover, a:active
+   {
+       color: inherit;
+   }
+   h2
+   {
+       padding-left: 70px;
+   }
+
+  </style>
+</head>
+
+<body>
+
+   <h1>CHUNGWA</h1>
+   <hr>
+     <div id="main-content">
+      <div class="article">
+      <h2>KILIMO CHA CHUNGWA</h2>
+      <img src="chungwaaaaa.jpg">
+      <p>Chungwa ni tunda la mchungwa. Jina la kisayansi ni Citrus aurantium L. var. sinensis L. au Citrus sinensis (L.) Osbeck kwenye familia ya Rutaceae. Tunda lifahamikalo kisayansi kama Citrus sinesis huitwa chungwa tamu ili kutofautisha na Citrus aurantium, chungwa chungu. Chungwa ni mahulusi kati ya pomelo (Citrus maxima) na mandarin (Citrus reticulata).
+
+Mti wake mdogo utoao maua hufikia urefu takribani mita kumi na majani yake yasiyokauka muda wote.
+
+Tunda lenyewe ni kijani na rangi inabadilika kuelekea njano ikiiva. Pamoja na limau, danzi, balungi na ndimu ni mojawapo kati ya matunda chungwa, jamii ya matunda ya citrus. Matunda yote jamii ya jenasi ya citrus, huzaliana. Matunda ya jamii hii huonwa kama ya aina ya beri sababu ya kuwa na mbegu nyingi, kuwa laini na nyama nyingi huku yakikua kutoka kwenye ovari moja.
+
+Siku hizi chungwa ni tunda linalovunwa zaidi kati ya matunda chungwa na ni kati ya matunda yanayovunwa sana duniani. <br>
+      </p>
+           </div>
+               <div class="article">
+               <h2 id="mahitaji">MAHITAJI</h2>
+               <img src="chungwa5.jpg">
+               <p>KANUNI SAHIHI ZA KILIMO BORA CHA MACHUNGWA :  Mchungwa ni jamii ya mlimao, lakini hustawi zaidi ukanda wa pwani wenye joto kiasi, mvua zaidi na udongo wenye rutuba. Hapa Tanzania mikoa kama Pwani, Dar es salaam, Tanga, Mtwara, Lindi, na Morogoro ni maarufu kwa kilimo hiki. Pia tunda la mchungwa huliwa na binadamu ili kuongeza vitamini mwilini.</p>
+               </div>
+          <div class="article">
+               <h2 id="magonjwa">MAGONJWA</h2>
+               <img src=".\imagesss\orangedis1.JPEG">&nbsp<img src=".\imagesss\orangedis2.JPEG">
+
+
+               <p>1 – VIDONDA KATIKA SHINA  <br>
+
+Huletwa na aina fulani ya ukungu. Dalili za ugonjwa huu nikutokea sehemu ya shina ambayo hubadilika rangi.
+<br>
+Dalili; Huongezeka ukubwa na baadae kutoa utomvu.
+
+Kudhibitisha; Hutokea sana sehemu iliyo na udongo ambao huatamisha maji.<br>
+
+ 
+
+2 – UGONJWA WA KUKAUKA MITI  <br>
+
+Husababishwa na virusi ambavyo husambazwa na chawa weusi.
+<br>
+Dalili; Sehemu ya shina hunyauka na ndani yake hutokea mipasuko pasuko, kisha mmea hudhoofika polepole na kutoa matunda madogo madogo, na majani hubadilika ghafla na kuwa ya njano.
+
+ 
+<br>
+3 – UGONJWA WA KUTOBADILIKA RANGI KWA MACHUNGWA YANAPOKOMAA
+<br>
+Hutokea hasa kwenye sehemu za miinuko ya kati na kusambazwa na vidudu viitwavyo psyllids, na vile vile kwa kuchukua vikonyo na kubebeshea kutoka kwenye miti inayoonyesha dalili za ugonjwa.
+<br>
+Dalili; Hutokea mabaka ya njano kwenye majani, pia matunda huwa ya ukali nay a njano sana.
+<br>
+Kuzuia: Sehemu ambazo ugonjwa unaweza kutokea angalia wadudu kama chawa. Hakikisha wadudu hawa hawashambulii miche kwenye kitalu. Wakiwepo, majani huonyesha kujikunja kwa kuweka mafundo sehemu ya juu. Vilevile epuka kuchukua vikonyo kutoka kwenye miti yenye dalili za ugonjwa huu.</p>
+<br>
+ 
+
+<p>WADUDU :
+<br>
+1. NZI WEUPE (WHITE FLY)
+<br>
+Hawa ni wadudu ambao hukaa kwenye majani ya mimea wakizaliana na kufyonza virutubisho vya mmea, na hii husababisha mmea kudumaa na kutoa mazao duni.
+<br>
+Kuzuia : Njia ya kiusalama zaidi ni kwa kupandisha shambani wadudu aina ya  Cales noacki ambao huwala hawa nzi weupe na kuzuia kusambaa kwao.<br>
+<br>
+ 
+
+2. WADUDU MAFUTA (APHIDS)
+<br>
+Hawa ni wadudu weusi ambao hukaa kwenye majani ya mimea wakizaliana na kufyonza virutubisho vya mmea, na hii pia husababisha mmea kudumaa na kutoa mazao duni.
+<br>
+Kuzuia: Tumia kiuwa wadudu (insectside).</p>
+               </div>
+          <div class="article">
+               <h2 id="masoko">MASOKO</h2>
+
+               <p>maelezo ya masoko</p>
+               </div>
+           <div class="article">
+               <h2 id="ziada">ZIADA</h2>
+
+               <p>Machungwa kwa kawaida hulimwa kwa ajili ya biashara na hulimwa maeneo mengi duniani. Wazalishaji wa machungwa wanaoongoza duniani ni Brazili, Marekani na Meksiko. Machungwa huathirika kwa haraka sana na ukungu hivyo huhitaji matunzo mazuri pindi joto dogo sana linapotegemewa</p>
+               </div>
+
+               </div>
+<div id="news">
+<div class="news-article">
+    <h3>SOMO</h3>
+    <ul>
+        <li><a href="#mahitaji"><h4>MAHITAJI</h4></a></li>
+        <li><a href="#magonjwa"><h4>MAGONJWA</h4></a></li>
+        <li><a href="#masoko"><h4>MASOKO</h4></a></li>
+        <li><a href="#ziada"><h4>ZIADA</h4></a></li>
+        <img src="oraa.jpg">
+    </ul>
+
+    </div>
+    </div>
+
+</body>
+
+</html>
