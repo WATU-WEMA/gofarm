bbaffc41dc261a56811230b11fe0d506be997dff
Waltertz <67097588+Waltertz@users.noreply.github.com>
2020-08-11 08:21:09 +0300

    Add files via upload

---
+++ kitunguu.php
@@ -0,0 +1,139 @@
+<?php
+session_start();
+echo '<br><a href="farmgo.php?action=logout" style="color: inherit;font-size:50px;">LOG OUT</a>'
+?>
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+  <title>VITUNGUU</title>
+  <style>
+  body
+  {
+      background-color: green;
+      background-repeat:no repeat;
+      background-size:cover;
+      color: white ;
+  }
+  h1
+  {
+      text-align: center;
+      color:white ;
+  }
+   #main-content
+    {
+            width: 780px;
+             float: left;
+     }
+   #news
+    {
+         margin-left: 20px;
+          float: left;
+         width: 180px;
+     }
+     ul li a
+     {
+         text-decoration: none;
+     }
+       li a,a:visited, a:hover, a:active
+   {
+       color: inherit;
+   }
+   h2
+   {
+       padding-left: 70px;
+   }
+
+  </style>
+</head>
+
+<body>
+
+   <h1>VITUNGUU</h1>
+   <hr>
+     <div id="main-content">
+      <div class="article">
+      <h2>KILIMO CHA VITUNGUU</h2>
+      <img src="kituuu1.jpg">
+      <p> Vitunguu ni mojawapo ya zo muhimu ambalo hulimwa kwa kiasi kikubwa katika maeneo mengi Tanzania. Zao la vitunguu hulimwa kama zao la bishara katika maeneo mbalimbali nchini na huwapatia watu kipato kikubwa ambacho huwasadia katika kutatua changamoto mbalimbali za kiuchumi na kusaidia katika kupambana na umasikini.
+
+Vitunguu hutumika kama kiungo muhimu cha kuongeza harufu nzuri na ladha katika vyakula. Hutumika katika utengenezaji wa supu, siki ,kachumbari nk. Majani ya vitunguu yana madini ya chokaa na wanga kwa wingi. Vilevile virutubisho vingine kama protini, chuma na ascorbic acidi hupatikana katika vitunguu.
+      </p>
+           </div>
+               <div class="article">
+               <h2 id="mahitaji">MAHITAJI</h2>
+               <img src="onplant.jpg">&nbsp<img src="onplant1.jpg">
+<p> Mazingira
+
+Vitunguu husitawi vizuri katika maeneo yenye miinuko kuanzia mita 800 hadi 1500 kutoka usawa wa bahari. Hupendelea hali ya ubaridi kiasi hususani wakati wa kuotesha mbegu kati ya nyuzi joto 13 hadi 24°C ingawa kwa miche iliyoko kwenye kitalu joto zuri ni la nyuzi 20 hadi 27°C. Joto la juu hufanikisha utengenezaji wa kitunguu na ukaukaji. Hivyo ni vema vitunguu vioteshwe wakati wa baridi ili viweze kuvunwa wakati wa Joto.
+
+Udongo: Huhitaji udongo tifutifu wenye rutuba nyingi usiotuamisha maji na usiopasukapasuka. Huhitai udongo ulio katika kipimo cha pH kati ya 6.0 na 7.0</p><br>
+               </div>
+          <div class="article">
+               <h2 id="magonjwa">MAGONJWA</h2>
+               <img src=".\imagesss\kitunguudis.JPEG">
+
+
+
+            <p>  Ubwiri vinyoya juu(powdery mildew) na wa chini (Downy mildew)<br>
+
+Huu husababishwa na ukungu na hupendelea hali ya unyevu nyevu. Hushambulia hushambulia majani ya mimea iliyo shamabani na hata miche michanga. Dalili zake Kwenye majani kunaonekana vidoa vyeupe hasa kwenye majani makuu ya mimea michanga. Ukungu mweupe unatokea na kuongezeka kwa haraka kwenye sehemu au nyakati za baridi. Viini huishi kwenye mabaki ya mimea ya vitunguu na huweza kupeperushwa mbali na upepo.
+
+Nyunyuzia dawa mojawapo zifuatazo kuzuia ukungu: - Dithane-M45, Ridomil, Topsin M70 na Cupric Hydroxide (Champion).
+<br><br>
+Ugonjwa wa doa la pinki (Purple blotch)<br>
+
+Huu nao ni ugonjwa uletwao na Ukungu na hushambulia majani, shingo na vitunguu vilivyokomaa. Huanza kama doa dogo linalozama ndani ya jani na kisha linaongezeka ukubwa na kufanya baka la rangi ya Zambarau ambalo huonekana katikati ya jani ambalo baadaye huwa jeusi. Shina lililoshambuliwa hulegea na kuanguka katika wiki ya tatu au ya nne baada ya dalili za ugonjwa kuonekana. Vitunguu vilivyokomaa huoza kuanzia shingoni na hugeuka rangi kuwa ya njano iliyochanganyika na nyekundu.
+
+Zuia ugonjwa huu kwa kufanya yafuatayo:- Kupanda aina ya vitunguu visivyoshambuliwazidi na ugonjwa huu (Red Creole) Kubadilisha mazaoVuna vitunguu kwa wakati sahihiMatumizi ya mbolea zenye calcium, phosphate and potassium hupunguza kuenea kwa ugonjwa huu. Kwa upande mwingine,matumizi ya mbolea ya nitrojeni kwa wingi au kwa kiasi pungufu uongeza mlipuko wa magonjwa.Viatilifu kama vile MancozebDithane-M45, Ridomil, Topsin M70 na Cupric Hydroxide (Champion).
+<br><br>
+Kuoza kwa shingo (Neckrot)<br>
+
+Husababishwa na ukungu na hupendelea vile vile hali ya unyevunyevu. Hushambulia zaidi vitunguu vilivyohifadhiwa. Ugonjwa huu husababisha vitunguu kuwa laini, hubonyea na huonekana kama vilivyopikwa. Huanzia kwenye shingo na baadaye huenea kwenye kitunguu chote na kufunikwa na ukungu wa kijivu.
+<br><br>
+Namna ya kuzuia:-<br>
+
+Vuna vitunguu kwa wakati sahihi Kubadilisha mazao Tumia njia sahihi wakati wa kuvuna, epuka kuchubua au kukikwaruza wakati wa kuvuna Kausha vitunguu vizuri baada ya kuvuna Hifadhi vitunguu sehemu zisizo na unyevu mwingi
+<br><br>
+ UDHIBITI WA WADUDU <br>
+
+Sata (Cutworms) Utitiri wa vitunguu (Onion thrips), Funza wa vitunguu (Onion maggots) na Minyoo wa vitunguu (Onion wireworms), Bungua weupe (White grub),ni miongoni mwa wadudu wasumbufu kwenye zao la kitunguu.
+
+Tumia dawa zilizothibitishwa na wataalamu.</p>
+               </div>
+          <div class="article">
+               <h2 id="masoko">MASOKO</h2>
+               <img src="kituuu2.jpg">
+
+               <p>maelezo ya masoko</p>
+               </div>
+           <div class="article">
+               <h2 id="ziada">ZIADA</h2>
+
+               <p> KUHIFADHI<br>
+
+Vitunguu vilivyovunwa vikaushwe vizuri kabla ya kuvihifadhi. Vihifadhiwe katika eneo lenye hewa ya kutosha, isiyokuwa na unyevu wala jua kali. Vitutnguu vihifadhiwe kwa kuvitandaza juu ya chanja au kwa kufunga mashina yake yaliyokauka vizuri kwa kuyaning’iniza sehemu ya kuhifadhia. Vitunguu vinaweza kuhifadhiwa kwa miezi sita au zaidi bila kuharibika.
+<br><br>
+Mavuno
+<br>
+Mavuno ya vitunguu yanategeme huduma za shamba pamoja nav hali ya hewa. Vitunguu pia hutofautiana katika mavuno kulingana na aina ya kitunguu. Kwa wastani vitunguu vinaweza kutoa mavuno kati ya tani 30 hadi 40 kwa hekta moja.</p>
+               </div>
+
+               </div>
+<div id="news">
+<div class="news-article">
+    <h3>SOMO</h3>
+    <ul>
+        <li><a href="#mahitaji"><h4>MAHITAJI</h4></a></li>
+        <li><a href="#magonjwa"><h4>MAGONJWA</h4></a></li>
+        <li><a href="#masoko"><h4>MASOKO</h4></a></li>
+        <li><a href="#ziada"><h4>ZIADA</h4></a></li>
+    </ul>
+
+    </div>
+    </div>
+
+</body>
+
+</html>
